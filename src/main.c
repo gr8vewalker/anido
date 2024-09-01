04ff157b1a0b274b2383f2c796ae87a554842ee6
gr8vewalker <ahmetflix@duck.com>
2024-09-01 15:32:49 +0300

    feat: searching

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@
 #define TEXT_COLOR "\x1b[38;2;20;235;201m"
 #define PROGRAM_COLOR "\x1b[38;2;235;201;20m"
 #define USER_COLOR "\x1b[38;2;201;20;235m"
+#define ERROR_COLOR "\x1b[38;2;234;21;66m"
 #define PRINT(...) printf(__VA_ARGS__)
 
 int input_number(long *number);
@@ -16,13 +17,16 @@ int input_number(long *number);
 int main(int argc, char **argv) {
     int retval = 0;
     animProvider *provider = NULL;
-    animPart *part = NULL;
+    animEntry *anime = NULL;
+    animPart *episode = NULL;
     animSource *source = NULL;
     size_t i;
 
+    // ------------ INIT ------------ 
     anim_initialize();
     parse_opts(argc, argv);
 
+    // ------------ PROVIDER ------------ 
     if (!PROVIDER) {
         size_t providers_size;
         animProvider *providers = anim_list_providers(&providers_size);
@@ -52,6 +56,8 @@ int main(int argc, char **argv) {
     PRINT(TEXT_COLOR "Selected provider: " PROGRAM_COLOR "%s\n",
           provider->name);
 
+    // ------------ SEARCH ------------ 
+    int cli_search = SEARCH != NULL;
     if (!SEARCH) {
         PRINT(TEXT_COLOR "What do you want to search: " USER_COLOR);
         SEARCH = calloc(1024, sizeof(char));
@@ -60,9 +66,46 @@ int main(int argc, char **argv) {
             retval = 1;
             goto end;
         }
+        // remove trailing newline 
+        SEARCH[strcspn(SEARCH, "\r\n")] = 0;
     }
 
     PRINT(TEXT_COLOR "Searching " PROGRAM_COLOR "%s\n", SEARCH);
+
+    size_t found = 0;
+    animEntry *animes;
+    if (anim_search(provider, SEARCH, &found, &animes) != 0) {
+        log_error("Search failed! Search string was %s", SEARCH);
+        retval = 1;
+        goto end;
+    }
+
+    if (found == 0) {
+        if (cli_search) {
+            log_warn("No search results found! Exiting...");
+            goto end;
+        } else {
+            PRINT(ERROR_COLOR "No search results!\n");
+            goto end; // TODO: get back to search section instead of exiting
+        }
+    }
+
+    if (!cli_search) {
+        PRINT(TEXT_COLOR "Search results: \n");
+        for (i = 0; i < found; ++i) {
+            PRINT(PROGRAM_COLOR "%zu - %s\n", i + 1, animes[i].name);
+        }
+        int success = -1;
+        long in = 0;
+        do {
+            PRINT(TEXT_COLOR "Select an episode: " USER_COLOR);
+        } while ((success = input_number(&in)) || in < 1 ||
+                 in > found);
+        anime = &animes[in - 1];
+    } else {
+        anime = animes;
+    }
+
 
 end:
     anim_cleanup();
