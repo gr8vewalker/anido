1dd50a67016fdb58c6c39ec46c05d18295ddac3c
gr8vewalker <ahmetflix@duck.com>
2024-09-06 17:13:33 +0300

    docs: write documentation for libanim headers

--- subprojects/libanim/include/libanim/anim.h
+++ subprojects/libanim/include/libanim/anim.h
@@ -66,26 +66,120 @@ typedef struct animProvider {
     void *data; // internal
 } animProvider;
 
+/**
+ * Initialize library's functionality. Providers and networking is initialized
+ * with this.
+ * Must called at start of program.
+ *
+ * @return 0 if success
+ */
 int anim_initialize();
+
+/**
+ * Cleans up networking and frees providers.
+ */
 void anim_cleanup();
 
+/**
+ * List available providers.
+ *
+ * @param size Pointer to get size of providers
+ * @return Providers array.
+ */
 animProvider *anim_list_providers(size_t *size);
+
+/**
+ * Get a specific provider.
+ *
+ * @param name Name of provider
+ * @param exact Exact match flag
+ * @return Provider found or NULL
+ */
 animProvider *anim_get_provider(char *name, int exact);
+
+/**
+ * Get filters for a provider.
+ *
+ * @param provider the Provider
+ * @param size Pointer to get size of filters
+ * @return newly allocated filters array
+ */
 animFilter *anim_get_filters(animProvider *provider, size_t *size);
 
+/**
+ * Search for entries in a provider.
+ *
+ * @param provider the Provider
+ * @param input Search input
+ * @param size Pointer to get size of entries
+ * @param entries Array pointer to get entries
+ * @return 0 if success
+ */
 int anim_search(animProvider *provider, const char *input, size_t *size,
                 animEntry **entries);
+
+/**
+ * Fill details(parts etc.) in an entry.
+ *
+ * @param provider the Provider
+ * @param entry the Entry
+ * @return 0 if success
+ */
 int anim_details(animProvider *provider, animEntry *entry);
+
+/**
+ * Fill sources in a part.
+ *
+ * @param provider the Provider
+ * @param part the Part
+ * @return 0 if success
+ */
 int anim_sources(animProvider *provider, animPart *part);
 
+/**
+ * Download a source to a file.
+ *
+ * @param source the Source
+ * @param path Path of download file
+ * @param tmp Temporary folder
+ * @return 0 if success
+ */
 int anim_download(animSource *source, const char *path, const char *tmp);
+
+/**
+ * Stream a source. Used for playback without downloading.
+ *
+ * @param source the Source
+ * @param result String pointer for streaming url/file
+ * @param tmp Temporary folder
+ * @return 0 if success
+ */
 int anim_stream(animSource *source, char **result, const char *tmp);
 
-// Freeing
-
-// anim_free_entries can be used to recursively free all of the entries.
+/**
+ * Frees entries.
+ * Also calls anim_free_parts for each entry.
+ *
+ * @param entries the Entries
+ * @param size Entry count
+ */
 void anim_free_entries(animEntry *entries, size_t size);
+
+/**
+ * Frees parts.
+ * Also calls anim_free_sources for each source.
+ *
+ * @param parts the Parts
+ * @param size Part count
+ */
 void anim_free_parts(animPart *parts, size_t size);
+
+/**
+ * Frees sources.
+ *
+ * @param sources the Sources
+ * @param size Source count
+ */
 void anim_free_sources(animSource *sources, size_t size);
 
 #endif
